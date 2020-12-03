2a9b4c376d3aacf535e80edb6d5cb28c2465f740
Filipe <filipebastias94@gmail.com>
2020-12-03 13:35:47 +0000

    Added classrooms class

---
+++ app/src/main/java/com/example/cienciassafe/Classrooms.java
@@ -0,0 +1,34 @@
+package com.example.cienciassafe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Classrooms extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    String[] buildings = { "Edifício C1", "Edifício C2", "Edifício C3", "Edifício C4", "Edifício C5", "Edifício C6", "Edifício C7", "Edifício C8" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classrooms);
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, buildings);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Selected option: "+buildings[position] , Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
+    }
+}
