11fa5e7234a62980975be8549a3e3e0ceee285c2
Diogo Frazao <doga.frazao@gmail.com>
2020-12-16 17:00:03 +0000

    Firabase, lista de salas, fragment settings, etc

--- app/src/main/java/com/example/cienciassafe/ClassroomsFragment.java
+++ app/src/main/java/com/example/cienciassafe/ClassroomsFragment.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,8 +27,7 @@ import android.widget.Toast;
  * create an instance of this fragment.
  */
 public class ClassroomsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
-
-    String[] buildings = { "Edifício C1", "Edifício C2", "Edifício C3", "Edifício C4", "Edifício C5", "Edifício C6", "Edifício C7", "Edifício C8" };
+    String[] buildings = null;
     private View v = null;
     public ClassroomsFragment() {
         // Required empty public constructor
@@ -44,6 +52,7 @@ public class ClassroomsFragment extends Fragment implements AdapterView.OnItemSe
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        buildings = getActivity().getResources().getStringArray(R.array.list_buildings);
         v = (View) inflater.inflate(R.layout.fragment_classrooms, container, false);
 
         if (v != null) {
@@ -63,6 +72,41 @@ public class ClassroomsFragment extends Fragment implements AdapterView.OnItemSe
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
         Toast.makeText(getActivity(), "Selected option: "+buildings[position] , Toast.LENGTH_SHORT).show();
+
+        final LinearLayout ll = (LinearLayout) v.findViewById(R.id.rooms);
+        ll.removeAllViews();
+
+        DatabaseReference reff;
+        reff = FirebaseDatabase.getInstance().getReference();
+
+
+
+        reff.child("rooms").child(buildings[position].toLowerCase()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    int i = 0;
+                    for(DataSnapshot d : dataSnapshot.getChildren()) {
+                        TextView Room = new TextView(getActivity());
+                        Room.setText(getActivity().getResources().getString(R.string.room)+" "+d.getKey().replace("-","."));
+                        Room.setId(i);
+                        Room.setHeight((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, getResources().getDisplayMetrics()));
+                        Room.setLayoutParams(new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT));
+                        ll.addView(Room);
+                        i++;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value. " + error.toException());
+            }
+        });
+
     }
 
     @Override
