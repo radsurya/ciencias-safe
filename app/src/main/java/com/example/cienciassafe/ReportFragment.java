ce99e9ed044608cae617caed32d71cef93ecf854
Diogo Frazao <doga.frazao@gmail.com>
2020-12-16 21:04:02 +0000

    qr code e webview

--- app/src/main/java/com/example/cienciassafe/ReportFragment.java
+++ app/src/main/java/com/example/cienciassafe/ReportFragment.java
@@ -1,12 +1,29 @@
 package com.example.cienciassafe;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Vibrator;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,15 +31,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ReportFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View v = null;
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView textView;
+    BarcodeDetector barcodeDetector;
 
     public ReportFragment() {
         // Required empty public constructor
@@ -39,26 +52,79 @@ public class ReportFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static ReportFragment newInstance(String param1, String param2) {
         ReportFragment fragment = new ReportFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        v = inflater.inflate(R.layout.fragment_report, container, false);
+
+        if (v != null) {
+            surfaceView = (SurfaceView) v.findViewById(R.id.camerapreview);
+            textView = (TextView) v.findViewById(R.id.textView);
+
+            barcodeDetector = new BarcodeDetector.Builder(getActivity())
+                    .setBarcodeFormats(Barcode.QR_CODE).build();
+
+            cameraSource = new CameraSource.Builder(getActivity(), barcodeDetector)
+                    .setRequestedPreviewSize(640, 480).build();
+
+            surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                    if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(holder);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                    cameraSource.stop();
+                }
+            });
+
+            barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(Detector.Detections<Barcode> detections) {
+                    final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                    if (qrCodes.size() != 0) {
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+                                vibrator.vibrate(1000);
+                                textView.setText(qrCodes.valueAt(0).displayValue);
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
+        return v;
     }
 }
