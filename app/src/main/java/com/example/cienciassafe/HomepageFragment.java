dcdaebf768bec60779387af80d8817c8f3108edf
Diogo Frazao <doga.frazao@gmail.com>
2020-12-16 23:32:26 +0000

    string default em português

--- app/src/main/java/com/example/cienciassafe/HomepageFragment.java
+++ app/src/main/java/com/example/cienciassafe/HomepageFragment.java
@@ -13,6 +13,7 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,9 @@ import android.widget.Button;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
@@ -28,6 +32,7 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -45,15 +50,6 @@ public class HomepageFragment extends Fragment {
     private View v = null;
     Button mainBluetoothBtn;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public HomepageFragment() {
         // Required empty public constructor
     }
@@ -62,27 +58,17 @@ public class HomepageFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment HomepageFragment.
      */
     // TODO: Rename and change types and number of parameters
     public static HomepageFragment newInstance(String param1, String param2) {
         HomepageFragment fragment = new HomepageFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
         System.out.println("-------------- TESTE 1");
     }
 
@@ -172,6 +158,7 @@ public class HomepageFragment extends Fragment {
         map = (MapView) v.findViewById(R.id.map);
 
         if (map != null) {
+
             map.setTileSource(TileSourceFactory.MAPNIK);
             map.setMultiTouchControls(true);
 
@@ -193,16 +180,17 @@ public class HomepageFragment extends Fragment {
                 return;
             }
             Location lastKnownLoc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            System.out.println("--------------before if");
             if (lastKnownLoc != null){
-                IMapController mapController = map.getController();
-                mapController.setZoom(20.5);
                 int longTemp = (int)(lastKnownLoc.getLongitude()* 1000000);
                 int latTemp = (int)(lastKnownLoc.getLatitude() * 1000000);
+                IMapController mapController = map.getController();
+                System.out.println("--------------ZOOM");
+                mapController.setZoom(10);
                 startPoint =  new GeoPoint(latTemp, longTemp);
                 mapController.setCenter(startPoint);
             }
         }
-
     }
 
 }
