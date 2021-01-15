979337b5d3b33831bd21523bf9224b806f5f4f6d
Radica Giva <fc44311@alunos.fc.ul.pt>
2021-01-15 16:23:30 +0000

    Read beacon and show in homepage

--- app/src/main/java/com/example/cienciassafe/MainActivity.java
+++ app/src/main/java/com/example/cienciassafe/MainActivity.java
@@ -12,39 +12,129 @@ import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.material.navigation.NavigationView;
-
-import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
 import java.util.ArrayList;
 
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.Requirement;
+import com.estimote.mustard.rx_goodness.rx_requirements_wizard.RequirementsWizardFactory;
+import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
+import com.estimote.proximity_sdk.api.ProximityObserver;
+import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
+import com.estimote.proximity_sdk.api.ProximityZone;
+import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
+import com.estimote.proximity_sdk.api.ProximityZoneContext;
+
+import java.util.List;
+import java.util.Set;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ProximityObserver proximityObserver;
+    private ProximityZone zone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Handle Estimote */
+        EstimoteCloudCredentials cloudCredentials = new EstimoteCloudCredentials("cienciassafe-bly", "57d3410e9b3c0594fe5ba8da9b1f5070");
+
+        // Create the Proximity Observer
+        this.proximityObserver =
+                new ProximityObserverBuilder(getApplicationContext(), cloudCredentials)
+                        .onError(new Function1<Throwable, Unit>() {
+                            @Override
+                            public Unit invoke(Throwable throwable) {
+                                // Log.e("app", "proximity observer error: " + throwable);
+                                return null;
+                            }
+                        })
+                        .withBalancedPowerMode()
+                        .build();
+
+        zone = new ProximityZoneBuilder()
+                .forTag("cienciassafe-bly")
+                // .inCustomRange(3.0)
+                .inNearRange()
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext context) {
+                        String beaconTitle = context.getAttachments().get("cienciassafe-bly/title");
+                        // TextView viewBeacon = findViewById(R.id.beaconTitle);
+                        // viewBeacon.setText("Room" + beaconTitle);
+                        // Log.d("app", "Welcome to " + deskOwner + "'s desk");
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext context) {
+                        // TextView viewBeacon = findViewById(R.id.beaconTitle);
+                        // viewBeacon.setText("Beacon test");
+                        // Log.d("app", "Bye bye, come again!");
+                        return null;
+                    }
+                })
+                .onContextChange(new Function1<Set<? extends ProximityZoneContext>, Unit>() {
+                    @Override
+                    public Unit invoke(Set<? extends ProximityZoneContext> contexts) {
+                        List<String> deskOwners = new ArrayList<>();
+                        for (ProximityZoneContext context : contexts) {
+                            String beaconTitle = context.getAttachments().get("cienciassafe-bly/title");
+                            deskOwners.add(beaconTitle);
+                        }
+                        TextView viewBeacon = findViewById(R.id.beaconTitle);
+                        switch (deskOwners.get(0)) {
+                            case "lemon":
+                                viewBeacon.setText("Sala 1.1.36 está cheia!");
+                            case "beetroot":
+                                viewBeacon.setText("Sala 1.2.21 está vazia!");
+                            case "candy":
+                                viewBeacon.setText("Sala 1.2.22 está quase cheia!");
+                        }
+
+
+                        Log.d("app", "In range of desks: " + deskOwners);
+                        return null;
+                    }
+                })
+                .build();
+
+        RequirementsWizardFactory
+                .createEstimoteRequirementsWizard()
+                .fulfillRequirements(this,
+                        // onRequirementsFulfilled
+                        new Function0<Unit>() {
+                            @Override public Unit invoke() {
+                                Log.d("app", "requirements fulfilled");
+                                proximityObserver.startObserving(zone);
+                                return null;
+                            }
+                        },
+                        // onRequirementsMissing
+                        new Function1<List<? extends Requirement>, Unit>() {
+                            @Override public Unit invoke(List<? extends Requirement> requirements) {
+                                Log.e("app", "requirements missing: " + requirements);
+                                return null;
+                            }
+                        },
+                        // onError
+                        new Function1<Throwable, Unit>() {
+                            @Override public Unit invoke(Throwable throwable) {
+                                Log.e("app", "requirements error: " + throwable);
+                                return null;
+                            }
+                        });
+        /* End -  Handle Estimote */
 
         /* Handle menu sidebar */
         final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
@@ -81,4 +171,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Menu.class);
         startActivity(intent);
     } */
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
