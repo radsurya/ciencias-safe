55e5bedbeb2aa8183390449ea48d426e001600ed
Diogo Frazao <doga.frazao@gmail.com>
2021-01-21 12:23:23 +0000

    Focar na FCUL

--- app/src/main/java/com/example/cienciassafe/HomepageFragment.java
+++ app/src/main/java/com/example/cienciassafe/HomepageFragment.java
@@ -2,37 +2,25 @@ package com.example.cienciassafe;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
-import org.osmdroid.events.MapListener;
-import org.osmdroid.events.ScrollEvent;
-import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link HomepageFragment#newInstance} factory method to
@@ -43,7 +31,6 @@ public class HomepageFragment extends Fragment {
     private Context context = null;
     private MapView map = null;
     private MyLocationNewOverlay mLocationOverlay;
-    // private CompassOverlay mCompassOverlay;
     private LocationManager lm;
     private GeoPoint startPoint;
     private View v = null;
@@ -145,34 +132,15 @@ public class HomepageFragment extends Fragment {
             map.setMultiTouchControls(true);
 
             //Shows user location using an arrow
-            this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context), map);
+            this.mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(context),map);
             this.mLocationOverlay.enableMyLocation();
             map.getOverlays().add(this.mLocationOverlay);
 
-            //Sets the start point of the map where user is located
-            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            Location lastKnownLoc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            System.out.println("--------------before if");
-            if (lastKnownLoc != null){
-                int longTemp = (int)(lastKnownLoc.getLongitude()* 1000000);
-                int latTemp = (int)(lastKnownLoc.getLatitude() * 1000000);
-                IMapController mapController = map.getController();
-                System.out.println("--------------ZOOM 20");
-                mapController.setZoom(20);
-                startPoint =  new GeoPoint(latTemp, longTemp);
-                mapController.setCenter(startPoint);
-            }
-
+            IMapController mapController = map.getController();
+            mapController.setZoom(19);
+            //FCUL
+            startPoint =  new GeoPoint(38.75619, -9.15660);
+            mapController.setCenter(startPoint);
         }
     }
 
